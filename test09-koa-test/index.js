0260f180dea5636cf46c50e9722a7bf3e497b9f6
yuxiaoyang03 <yuxiaoyang03@baidu.com>
2019-11-06 21:48:50 +0800

    how koa2 to work?

---
+++ test09-koa-test/index.js
@@ -0,0 +1,42 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  console.log('1.....第一层洋葱--开始...')
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+  console.log('2.....第一层洋葱--结束...')
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  console.log('3.......第二层洋葱--开始.')
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+  console.log('4.....第二层洋葱--结束...')
+});
+
+// response
+
+app.use(async ctx => {
+  console.log('5.......第三层洋葱--开始')
+  ctx.body = 'Hello World';
+  console.log('6.......第三层洋葱--结束')
+});
+
+app.listen(3000);
+
+/** koa -- 洋葱卷模型
+1.....第一层洋葱--开始...
+3.......第二层洋葱--开始.
+5.......第三层洋葱--开始
+6.......第三层洋葱--结束
+4.....第二层洋葱--结束...
+2.....第一层洋葱--结束...
+ */
